251329960149498761bb660443b530d5caf64c28
pjramg <pjramg@gmail.com>
2019-07-26 08:25:57 -0400

    Edited-07/26/2019
    some chages were done to takeImage.py: take image each 5 minutes (for testing cameras). Also, They were included another files: autoboot.txt (to save in RECOVERY partition when whole system is configured), camera.txt (IP camera), rc_local_setup.txt (it includes command line to add in rc.local operative system file), wittySetup.txt (it includes command lines to setup wittyPi)

--- Codes/takeImage.py
+++ Codes/takeImage.py
@@ -3,7 +3,7 @@
 from picamera import PiCamera
 from time import sleep
 import datetime
-from checkSpace import getAvail
+#from checkSpace import getAvail
 
 camera = PiCamera()
 
@@ -13,23 +13,27 @@ for i in range(1000):
     curr = datetime.datetime.now()
     #print("Curr hour = ",curr.hour)
     
-    if curr.hour >= 17:
+    if curr.hour >= 21:
         sleep(12*60*60)
     else:
-        if(getAvail() < 5):
-            print("MEMORY FULL...!")
-            break
-        #print("Inside else")
+    #    if(getAvail() < 5):
+    #        f = open("/home/pi/logs.txt","a")
+    #        s = "The timestamp is: " + str(curr) + " | i:" + str(i) + "ERROR: MEMORY FULL."
+    #        f.write(s + "\n")
+    #        f.close()
+    #        print("MEMORY FULL...!")
+    #        break
+    #    print("Inside else")
         
         camera.start_preview(fullscreen=False, window=(100,100,640,640))
         sleep(5)
-        camera.capture('/home/pi/Desktop/images/' + curr.strftime("%d_%m_%Y_%H:%M:%S") + '.jpg')
+        camera.capture('/home/pi/images/' + curr.strftime("%d_%m_%Y_%H_%M_%S") + '.jpg')
         camera.stop_preview()
 
-        f = open("/home/pi/Desktop/logs.txt","a")
+        f = open("/home/pi/logs.txt","a")
         s = "The timestamp is: " + str(curr) + " | i:" + str(i)
         f.write(s + "\n")
         f.close()
-        sleep(30*60 - 5)
+        sleep(5*60 - 5)
 
 
